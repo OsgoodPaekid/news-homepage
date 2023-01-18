0ebec99f03841e7eab7a29ca9d49047bf48fbcdf
Osgood <osgoodackom@gmail.com>
2023-01-18 12:07:32 +0000

    worked on responsiveness

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const iconMenu = document.querySelector(".icon-menu")
 const menu = document.querySelector(".menu")
 const showIconMenu = document.querySelector(".show-icon-menu")
 
-
 showIconMenu.addEventListener("click", function(){
     menu.classList.toggle("icon-menu")
+   
 })
